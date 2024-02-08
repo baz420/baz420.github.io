d80c68fc6871ea003b5ea261cc5aff137363d008
paul marsh <bazinator420@gmail.com>
2024-02-09 02:48:52 +1100

    10

--- src/index.js
+++ src/index.js
@@ -2,31 +2,10 @@
 
 import * as a1lib from "@alt1/base";
 
-
-     const pageTitleEl = document.querySelector("#title")
-    // const val = a1lib.getMousePosition()
-    const imgEl = document.getElementById("img-el");
-
-    // pageTitleEl.textContent = `X: ${val.x}, Y: ${val.y}`
      
 
- 
-     var mainImgref = a1lib.capture(alt1.screenX,alt1.screenY,1000, 1000);
-
-    // var imagebuffer = imgref.toDrawableData()
 
 
-    
-    // imagebuffer.show()
-
-    const doThings = async() => {
-        const imageData = await a1lib.ImageDetect.imageDataFromUrl("./yo.png")
-        const x = await mainImgref.pixelCompare(imageData, 0, 0, 900)
-        console.log(x)
-    }
-
-
-    
         var imgref = a1lib.capture(100,100,400,400);
 
 // Retrieve our raw pixel data so we can directly read it
