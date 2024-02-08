eb1bc7037bb9bc520127cfaf4a4112642362f2a5
paul marsh <bazinator420@gmail.com>
2024-02-09 04:42:49 +1100

    24

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import * as a1lib from "@alt1/base"
 
 const imgContainer = document.getElementById("img-container")
- const imageData = a1lib.capture(100,100,400,400);
- const imageBuffer = imageData.toDrawableData();
- const imgCanvas = imageBuffer.show();
+ var imageData = a1lib.capture(100,100,400,400);
+ var imageBuffer = imageData.toImage();
+ var imgCanvas = imageBuffer.show();
 
  imgContainer.appendChild(imgCanvas);
 
