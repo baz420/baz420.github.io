998d551c93b6da35a62ea85c2018aa4d11d02b00
paul marsh <bazinator420@gmail.com>
2024-02-09 04:37:04 +1100

    22

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         main: path.resolve(__dirname, "src/index.js")
     },
     output: {
-        path: path.resolve(__dirname, "docs"),
+        path: path.resolve(__dirname, "dist"),
         filename: "[name].js"
     }
 }
