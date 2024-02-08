264e61db0f1ad3b93953e8ae6dc950f77c5c5f89
paul marsh <bazinator420@gmail.com>
2024-02-09 03:05:36 +1100

    10

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
+import * as a1lib from "@alt1/base"
 
 
-import * as a1lib from "@alt1/base";
+console.log(a1lib.getMousePosition());
 
-     
-
-
-
-        var imgref = a1lib.capture(100,100,400,400);
-
-// Retrieve our raw pixel data so we can directly read it
-        var imagebuffer = imgref.toData();
-
-// Show the image by adding it to the DOM (for debugging)
-       
-        imagebuffer.show();
-
-    
-    // pixelCompare(imageData, 0, 0, 900)
 
 
 
